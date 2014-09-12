13070f0d6c2b73f99f94e3ce72e6d340aa969f34
mala23 <mail@mala23.me>
2014-09-12 20:02:27 +0700

    Array for students

--- directory.rb
+++ directory.rb
@@ -1,37 +1,72 @@
 student_count = 9
-#First we print the list of students
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-puts "ecomba"
-puts "stephenlloyd"
-puts "alexpeattie"
-puts "vic329"
-puts "NicolePell"
-puts "bmordan"
-puts "elenagarrone"
-puts "yvettecook"
-puts "HatStephens"
-puts "ananogal"
-puts "craigh44"
-puts "Scully87"
-puts "EllaNancyFay"
-puts "fadieh"
-puts "mala23"
-puts "zrasool88"
-puts "galicians"
-puts "danjocutler"
-puts "camillavk"
-puts "MadameSardine"
-puts "jamesascarter"
-puts "slstevens"
-puts "Schlap"
-puts "andrewhercules"
-puts "shortynielsen"
-puts "snozza"
-puts "SBLLB"
-puts "annaschechter"
-puts "alexfakhri"
-puts "deniseyu"
-puts "AndrewHarrison"
+#Let's put all students intod an array
+students = [
+
+ "ecomba",
+ "stephenlloyd",
+ "alexpeattie",
+ "vic329",
+ "NicolePell",
+ "bmordan",
+ "elenagarrone",
+ "yvettecook",
+ "HatStephens",
+ "ananogal",
+ "craigh44",
+ "Scully87",
+ "EllaNancyFay",
+ "fadieh",
+ "mala23",
+ "zrasool88",
+ "galicians",
+ "danjocutler",
+ "camillavk",
+ "MadameSardine",
+ "jamesascarter",
+ "slstevens",
+ "Schlap",
+ "andrewhercules",
+ "shortynielsen",
+ "snozza",
+ "SBLLB",
+ "annaschechter",
+ "alexfakhri",
+ "deniseyu",
+ "AndrewHarrison"
+]
+#And then print them
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[12]
+puts students[13]
+puts students[14]
+puts students[15]
+puts students[16]
+puts students[17]
+puts students[18]
+puts students[19]
+puts students[20]
+puts students[21]
+puts students[22]
+puts students[23]
+puts students[24]
+puts students[25]
+puts students[26]
+puts students[27]
+puts students[28]
+puts students[29]
+puts students[30]
+puts students[31]
+puts students[32]
+puts students[33]
+puts students[34]
 #Finally, we print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
