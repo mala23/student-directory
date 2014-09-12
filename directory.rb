65fd1eae17a447579e3266791b5cf7fd970b42b8
mala23 <mail@mala23.me>
2014-09-12 20:40:18 +0700

    Separate data from operation

--- directory.rb
+++ directory.rb
@@ -1,7 +1,5 @@
-student_count = 9
 #Let's put all students intod an array
 students = [
-
  "ecomba",
  "stephenlloyd",
  "alexpeattie",
@@ -35,38 +33,10 @@ students = [
  "AndrewHarrison"
 ]
 #And then print them
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts students[12]
-puts students[13]
-puts students[14]
-puts students[15]
-puts students[16]
-puts students[17]
-puts students[18]
-puts students[19]
-puts students[20]
-puts students[21]
-puts students[22]
-puts students[23]
-puts students[24]
-puts students[25]
-puts students[26]
-puts students[27]
-puts students[28]
-puts students[29]
-puts students[30]
-puts students[31]
-puts students[32]
-puts students[33]
-puts students[34]
+puts "The students of my cohort at Makers Academy"
+puts "-------------"
+students.each do |student|
+  puts student
+end
 #Finally, we print the total
 puts "Overall, we have #{students.length} great students"
